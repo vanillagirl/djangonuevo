ada7e9db7c9b6ef1b57bc23352c6fddd482a00e9
Ingrid <ingrid@ingrid.com>
2018-11-05 04:35:08 -0600

    Noveno commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^accounts/login/$', views.login, name='login'),
+    re_path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    re_path(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    re_path(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
